a0e6417339afd214f12d9739459a7a535afde8f5
123 <van.moshk@gmail.com>
2024-11-14 02:32:12 +0300

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 use pest::Parser;
 use pest_derive::Parser;
 use std::fs;
-#[derive(Parser)]
-#[grammar = "gramatyka.pest"] 
-pub struct Grammar;
+use csv_parser_moshkovskyi::parse_csv;
 
 fn main() {
-    let file_content = fs::read_to_string("C:/Haskell/parser_moshkovskyi_ivan/src/data.csv").expect("Unable to read file");
-    
-    match Grammar::parse(Rule::csv, &file_content) {
-        Ok(parsed) => {
-            for record in parsed {
-                println!("{:?}", record);
-            }
-        }
-        Err(e) => println!("Parsing failed: {}", e),
+    let file_content = fs::read_to_string("src/data.csv").expect("Unable to read file");
+    match parse_csv(&file_content) {
+        Ok(_) => println!("CSV parsed successfully!"),
+        Err(e) => eprintln!("Error parsing CSV: {}", e),
     }
+    // match Grammar::parse(Rule::csv, &file_content) {
+    //     Ok(parsed) => {
+    //         for record in parsed {
+    //             println!("{:?}", record);
+    //         }
+    //     }
+    //     Err(e) => println!("Parsing failed: {}", e),
+    // }
 }
